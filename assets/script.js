65f1e8ea24fa6edbf6f995cd42dc5746e26a3dc5
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-14 16:12:41 +0100

    js ex 06 part 02

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+﻿function mouseOver(picture) {
+    if (picture.id == "image1") {
+        document.getElementById("image1").src = "assets/img/image1_2.jpg";
+    }
+    else if (picture.id == "image2") {
+        document.getElementById("image2").src = "assets/img/image2_2.jpg";
+    }
+    else if (picture.id == "image3") {
+        document.getElementById("image3").src = "assets/img/image3_2.jpg";
+    }
+    else if (picture.id == "image4") {
+        document.getElementById("image4").src = "assets/img/image4_2.jpg";
+    }
+    else {
+        document.getElementById("image5").src = "assets/img/image5_2.jpg";
+    }
+}
+function resetImg(picture) {
+    if (picture.id == "image1") {
+        document.getElementById("image1").src = "assets/img/image1.jpg";
+    }
+    else if (picture.id == "image2") {
+        document.getElementById("image2").src = "assets/img/image2.jpg";
+    }
+    else if (picture.id == "image3") {
+        document.getElementById("image3").src = "assets/img/image3.jpg";
+    }
+    else if (picture.id == "image4") {
+        document.getElementById("image4").src = "assets/img/image4.jpg";
+    }
+    else {
+        document.getElementById("image5").src = "assets/img/image5.jpg";
+    }
+}
